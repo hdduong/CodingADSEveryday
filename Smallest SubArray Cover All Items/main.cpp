c108149e11b4b0049473693f6734e21365961619
hdduong <hdduong.at.wpi.edu@gmail.com>
2015-03-08 18:08:58 -0700

    Imporve on EPI 13.9

--- Smallest SubArray Cover All Items/main.cpp
+++ Smallest SubArray Cover All Items/main.cpp
@@ -28,6 +28,17 @@ bool  seeAllElement(unordered_map<string,int>& mapQ) {
 }
 
 
+void GetMinMaxIndex(unordered_map<string,int>& mapQ, int& minIndex, int& maxIndex) {
+	minIndex = INT_MAX;
+	maxIndex = INT_MIN;
+
+	for (unordered_map<string,int>::iterator it = mapQ.begin(); it != mapQ.end(); it++) {
+		if (minIndex > it->second) minIndex = it->second;
+		if (maxIndex < it->second) maxIndex = it->second;
+	}
+	
+}
+
 // Q is small array
 int findSmallestSubString(vector<string>& A, vector<string>& Q) {
 	unordered_map<string, int> mapQ;
@@ -122,12 +133,65 @@ int findSmallestSubString(vector<string>& A, vector<string>& Q) {
 	return minLen;
 }
 
+// the idea is keep track last Element.
+// okie to loop to find Min and Max Index
+
+int FindSmallestSubString2(vector<string> vA, vector<string> vQ) {
+	unordered_map<string, int> mapQ;
+
+	for (vector<string>::iterator itQ = vQ.begin(); itQ!= vQ.end(); itQ++) {
+		mapQ.emplace(*itQ,-1);
+	}
+	
+	int currMinIndex = -1;
+	int currMaxIndex = -1;
+	int preMaxIndex = -1;
+	int preMinIndex = -1;
+	int allMinIndex = -1;
+	int allMaxIndex = -1;
+	int atAIndex = 0;
+
+	for (vector<string>::iterator itA = vA.begin(); itA!=vA.end(); itA++) {
+			
+		unordered_map<string, int>::iterator itMapQ = mapQ.find(*itA);
+		
+		if (itMapQ != mapQ.end()) { // found 
+			
+			itMapQ->second = atAIndex;
+			if (!seeAllElement(mapQ)) {
+				
+			} else {
+				preMinIndex = currMinIndex;
+				preMaxIndex = currMaxIndex;
+				
+				// accept to loop Min Max here since user input not many
+				// search words
+				GetMinMaxIndex(mapQ,currMinIndex, currMaxIndex);
+
+				if ( (currMaxIndex - currMinIndex < preMaxIndex - preMinIndex) ||
+					((preMaxIndex == -1) && (preMinIndex == -1)) )
+				{
+					allMinIndex = currMinIndex;
+					allMaxIndex = currMaxIndex;
+				}
+			}
+
+		}
+		atAIndex++;
+	}
+	if (allMaxIndex == -1 || allMinIndex == -1) return 0;
+
+	return (allMaxIndex - allMinIndex + 1);
+
+}
+
+
 int main() {
-	string arrA[] = {"a","b"," ","c"};
+	//string arrA[] = {"a","b"," ","c"};
 	//string arrA[] = {"a"," "," ","a","b"," ","c"};
 	//string arrA[] = {"a"," "," ","a","b"," ","c"," ","b","c","a"};
 	//string arrA[] = {"a"," "," ","a","b"," ","c"," ","c","a","b"};
-	//string arrA[] = {"a"," "," ","a","b"," ","c"," ","a","c","b"};
+	string arrA[] = {"a"," "," ","a","b"," ","c"," ","a","c","b"};
 	string arrQ[] = {"a","b","c"};
 	
 	vector<string> A(std::begin(arrA), std::end(arrA));
@@ -135,5 +199,10 @@ int main() {
 
 	int result = findSmallestSubString(A,Q);
 	cout << result << endl;
+	
+	int result2 = FindSmallestSubString2(A,Q);
+	cout << result2 << endl;
+	
+	
 	return 0;
 }
